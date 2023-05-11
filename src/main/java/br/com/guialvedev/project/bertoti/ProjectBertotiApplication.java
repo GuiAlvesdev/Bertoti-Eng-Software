90f9e5283db04ade9c1d43c91371c0d6253c0e7d
GuiAlvesdev <guialves1353@gmail.com>
2023-05-11 16:51:20 -0300

    fist commit

---
+++ src/main/java/br/com/guialvedev/project/bertoti/ProjectBertotiApplication.java
@@ -0,0 +1,13 @@
+package br.com.guialvedev.project.bertoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBertotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBertotiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guialvedev/project/bertoti/ProjectBertotiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guialvedev.project.bertoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBertotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
