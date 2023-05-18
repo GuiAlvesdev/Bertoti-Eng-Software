501ca97bbe26cbab7655762f14826d8bd41e8e8b
GuiAlvesdev <guialves1353@gmail.com>
2023-05-17 22:30:14 -0300

    finalizado navbar e index principal

---
+++ src/main/java/br/com/guialvedev/project/bertoti/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package br.com.guialvedev.project.bertoti.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/index");
+        return mv;
+
+
+    }
+}
